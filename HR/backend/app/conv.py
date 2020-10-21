084f37069e54608b710d5c27e1dec4de8263c1f5
AdamPudil <104brutus104@gmail.com>
2020-10-21 23:59:21 +0200

    fix

--- HR/backend/app/conv.py
+++ HR/backend/app/conv.py
@@ -11,7 +11,7 @@ def pos_to_city(lat, lng):
         return "Pracujeme na tom"
     string = string[0].split()
     string.pop(0)
-    return string.join
+    return string.join()
 
 
 # print(pos_to_city(48.858093, 2.294694))
