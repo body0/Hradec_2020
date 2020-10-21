e25bac8ad6cc7281b98c07df7848618c1a768f75
Kristýna Petrlíková <kalivtrope@gmail.com>
2020-10-21 23:32:19 +0200

    complete JSON return values

--- HR/backend/app/routes.py
+++ HR/backend/app/routes.py
@@ -1,7 +1,7 @@
 from app import app, db_session
 from flask import request, jsonify
 from app.models import Population, Cases
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 from app.prediction import calculate_R, pessimistic_prediction,\
      optimistic_prediction, days_to_predict
 
@@ -15,10 +15,29 @@ def query_records():
     today = datetime(2020, 10, 21)
     cases = db_session.query(Cases).filter_by(obec_kod=city.LAU_2)
     cases_now = cases.filter_by(datum=today).first()
-    cases_prev = [c[0] for c in reversed(list(cases.order_by(Cases.datum.desc()).
-                                           limit(7).
-                                           values('aktualne_nemocnych')))]
-    print(cases_prev)
+    cases_prev = [c[0] for c in list(cases.order_by(Cases.datum.desc()).
+                                     limit(14).
+                                     values('nove_pripady'))][1:]
+    r_pred = calculate_R(cases_prev)
+    print(cases_prev, r_pred)
+    pes_pred = pessimistic_prediction(cases_prev, r_pred)
+    opt_pred = optimistic_prediction(cases_prev, r_pred)
+
+    cf0 = []
+    curr = cases_now.aktualne_nemocnych
+    for i, inc in enumerate(pes_pred):
+        curr += inc
+        cf0.append({"date": today + timedelta(days=i+1),
+                    "valueAbsolute": curr,
+                    "valueRelative": inc})
+    cf1 = []
+    curr = cases_now.aktualne_nemocnych
+    for i, inc in enumerate(opt_pred):
+        curr += inc
+        cf1.append({"date": today + timedelta(days=i+1),
+                    "valueAbsolute": curr,
+                    "valueRelative": inc})
+    # print(r_pred, pes_pred, opt_pred)
     return jsonify({
         "name": city.nazev_obce,
         "population": city.obyv_celkem,
@@ -26,7 +45,10 @@ def query_records():
                 "date": today,
                 "valueAbsolute": cases_now.aktualne_nemocnych,
                 "valueRelative": cases_now.nove_pripady
-            }
+        },
+        "caseFuture0": cf0,
+        "caseFuture1": cf1,
+        "r": r_pred
     })
 
 
