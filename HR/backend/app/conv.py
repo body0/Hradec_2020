86ea7966cd139cc9c104ec68b107dd5ca55dbd0d
AdamPudil <104brutus104@gmail.com>
2020-10-21 21:59:33 +0200

    update

--- HR/backend/app/conv.py
+++ HR/backend/app/conv.py
@@ -1,7 +1,12 @@
 import requests
 
+key = "AIzaSyC2W0Vw4sTjRortHkmPg-G4qcTRWkjazAQ"; 
 
 def pos_to_city(lat, lng):
-    url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&location_type=ROOFTOP&result_type=street_address&key=AIzaSyC2W0Vw4sTjRortHkmPg-G4qcTRWkjazAQ"
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&location_type=ROOFTOP&result_type=street_address&key={key}"
     response = requests.get(url)
-    return response.json()
+    json = response.json()
+    return(json["plus_code"])
+
+
+print(pos_to_city(50.060148, 15.704721))
