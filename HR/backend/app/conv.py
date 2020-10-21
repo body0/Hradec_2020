a33ab15b5a5a87cbe1c0948e5aca36addac00266
AdamPudil <104brutus104@gmail.com>
2020-10-21 22:32:04 +0200

    Rybitvi

--- HR/backend/app/conv.py
+++ HR/backend/app/conv.py
@@ -6,7 +6,9 @@ def pos_to_city(lat, lng):
     url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&location_type=ROOFTOP&result_type=street_address&key={key}"
     response = requests.get(url)
     json = response.json()
-    return(json["plus_code"])
+    string = str(json["plus_code"]["compound_code"]).split(", ")
+    string = string[0].split()
+    return string[1]
 
 
 print(pos_to_city(50.060148, 15.704721))
