3f2c869c7627ee9fb1297728797f1927a0fdbcf2
Kristýna Petrlíková <kalivtrope@gmail.com>
2020-10-22 03:17:42 +0200

    add flask_cors

--- HR/backend/app/__init__.py
+++ HR/backend/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from sqlalchemy import create_engine
+from flask_cors import CORS
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker, Query, relationship, backref
 from config import Config
@@ -12,6 +13,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 db_session = scoped_session(sessionmaker(bind=engine))
+CORS(app)
 
 
 from app import routes, models, prediction
