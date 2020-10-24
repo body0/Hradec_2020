1365fd3007308cae5b099bf01710269e7390100a
Jan Nykl <nykl.jan.3693@gmail.com>
2020-10-24 16:46:58 +0200

    auto commint

--- DOCKER/db/init/setup.sh
+++ DOCKER/db/init/setup.sh
@@ -3,10 +3,10 @@ ODKAZ_VYSKYTY="https://share.uzis.cz/s/dCZBiARJ27ayeoS/download"
 ODKAZ_POPULACE="https://www.czso.cz/documents/10180/91917344/1300721903.xlsx/e7e94cb2-219c-4c6d-a46a-0f51d6891a5d?version=1.0"
 
 
-wget $ODKAZ_VYSKYTY -O vyskyty.zip
+wget $ODKAZ_VYSKYTY -O vyskyty.zip -q --show-progress
 unzip vyskyty.zip
 
-wget $ODKAZ_POPULACE -O populace.xlsx
+wget $ODKAZ_POPULACE -O populace.xlsx -q --show-progress
 
 
 # python3 parseTables.py
