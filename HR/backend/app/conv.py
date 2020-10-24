5a83bf6d97aad8773f01847eba72591fb192b611
AdamPudil <104brutus104@gmail.com>
2020-10-24 18:17:14 +0200

    fix prague

--- HR/backend/app/conv.py
+++ HR/backend/app/conv.py
@@ -2,15 +2,27 @@ import requests
 
 key = "AIzaSyC2W0Vw4sTjRortHkmPg-G4qcTRWkjazAQ";
 
+def delete_code(string):
+    string = string.split()
+    string.pop(0)
+    string = " ".join(string)
+    return(string)
+
 def pos_to_city(lat, lng):
     url = f"https://maps.googleapis.com/maps/api/geocode/json?latlng={lat},{lng}&location_type=ROOFTOP&result_type=street_address&key={key}"
     response = requests.get(url)
     json = response.json()
-    string = str(json["plus_code"]["compound_code"]).split(", ")
+
+    string = str(json["plus_code"]["compound_code"])    
+    string = delete_code(string)
+    string = string.split(", ")
+
     if(string[1] != "Czechia"):
         return "Pracujeme na tom"
-    string = string[0].split()
-    string.pop(0)
-    return(" ".join(string))
-
-print(pos_to_city(50.587286, 15.156801))
+ 
+    if(string[0] == "Prague"):
+        return("Praha")
+    else:
+        return(string[0])
+    
+#print(pos_to_city(50.07608,14.425262))
